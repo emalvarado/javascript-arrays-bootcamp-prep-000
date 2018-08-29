17feec879a9f1fe45738a67c9a104662b8de3b16
ELIZABETH ALVARADO <lily21418@yahoo.com>
2018-08-29 04:50:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,9 +22,11 @@ function accessElementInArray(array, index) {
   return array[index]
 }
 
-function destructivelyRemoveElementFromBeginningOfArray(array, element) {
+function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
   return array
 }
 
-function 
+function removeElementFromBeginningOfArray(array) {
+  
+}
